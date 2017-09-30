836263f1790f88b10e7534a2c552efb0cc02fbf6
Tom Wu <tom76kimo@yahoo-inc.com>
2017-09-30 18:39:20 +0800

    Add new option - minimize to support outputting minimized css file

--- example/webpack.config.js
+++ example/webpack.config.js
@@ -1,15 +1,24 @@
+var path = require('path');
+
 module.exports = {
     entry: __dirname + '/index.js',
     output: {
         path: __dirname,
-        filename: 'bundle.js',
+        filename: 'bundle.js'
     },
     module: {
         loaders: [
             {
                 test: /\.jsx?$/,
                 exclude: /(node_modules)/,
-                loader: '../dist/atomicLoader?configPath[]=' + __dirname + '/atomCssConfig.js&configPath[]=' + __dirname + '/atomCssConfig2.js'
+                loader: '../dist/atomicLoader',
+                query: {
+                    minimize: true,
+                    configPath: [
+                        path.resolve('./atomCssConfig.js'),
+                        path.resolve('./atomCssConfig2.js')
+                    ]
+                }
             },
             {
                 test: /\.jsx?$/,
